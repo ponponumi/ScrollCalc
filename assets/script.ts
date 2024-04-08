88129b8575cfa14df5b54f49b63b8da2f3741b7e
ponponumi <ponponumi@gmail.com>
2024-04-09 01:06:13 +0900

    1つしか追加されない問題を修正

--- assets/script.ts
+++ assets/script.ts
@@ -2,9 +2,9 @@ import ScrollCalc from "../src/ScrollCalc";
 
 // テキストリストを作成
 let textList = document.getElementById("textList");
-let text = document.createElement("li");
-text.textContent = "Hello World";
 
 for (let i = 0; i < 50; i++){
+  let text = document.createElement("li");
+  text.textContent = "Hello World";
   textList.appendChild(text);
 }
