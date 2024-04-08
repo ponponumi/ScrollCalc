f88ab9dcbeb8da3c3c0c2d2bcd5417b9d59de290
ponponumi <ponponumi@gmail.com>
2024-04-09 01:07:01 +0900

    リストがあるかどうか判定するように変更

--- assets/script.ts
+++ assets/script.ts
@@ -3,8 +3,10 @@ import ScrollCalc from "../src/ScrollCalc";
 // テキストリストを作成
 let textList = document.getElementById("textList");
 
-for (let i = 0; i < 50; i++){
-  let text = document.createElement("li");
-  text.textContent = "Hello World";
-  textList.appendChild(text);
+if (textList) {
+  for (let i = 0; i < 50; i++){
+    let text = document.createElement("li");
+    text.textContent = "Hello World";
+    textList.appendChild(text);
+  }
 }
