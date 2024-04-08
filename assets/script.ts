4c18c71b8acad4706fd67ed8b376dbf309c5f15d
ponponumi <ponponumi@gmail.com>
2024-04-09 01:05:08 +0900

    テキストリストを作成するコードを追加

--- assets/script.ts
+++ assets/script.ts
@@ -1,1 +1,10 @@
 import ScrollCalc from "../src/ScrollCalc";
+
+// テキストリストを作成
+let textList = document.getElementById("textList");
+let text = document.createElement("li");
+text.textContent = "Hello World";
+
+for (let i = 0; i < 50; i++){
+  textList.appendChild(text);
+}
