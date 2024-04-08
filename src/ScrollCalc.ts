5a19bc6a62ca9571f7b4f6663870d3e2f820a33d
ponponumi <ponponumi@gmail.com>
2024-04-09 01:09:17 +0900

    ピクセルを数値に変換するメソッドを作成

--- src/ScrollCalc.ts
+++ src/ScrollCalc.ts
@@ -1,5 +1,10 @@
 class ScrollCalc{
-  //
+  pixelNumber(pixel: string) {
+    // ピクセルを数値に変換
+    pixel = pixel.replace("px", "");
+    let result: number = Number(pixel);
+    return result;
+  }
 }
 
 export default new ScrollCalc();
