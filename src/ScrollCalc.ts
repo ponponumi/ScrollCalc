9082393239ce22289aca9d0f59f0bf89e65d17a8
ponponumi <ponponumi@gmail.com>
2024-04-09 01:16:14 +0900

    計算のコア機能を作成

--- src/ScrollCalc.ts
+++ src/ScrollCalc.ts
@@ -19,6 +19,44 @@ class ScrollCalc{
 
     return result;
   }
+
+  calcCore() {
+    // 計算のコア機能
+    // HTMLの高さ
+    let htmlHeight: number = this.htmlHeight();
+
+    // 画面の高さ
+    let windowY = window.innerHeight;
+
+    // スクロールエリア(HTMLの高さから画面の高さを引いた範囲)
+    let scrollArea: number = htmlHeight - windowY;
+
+    if (scrollArea < 0) {
+      // スクロールが0より小さければ、0に上書き
+      scrollArea = 0;
+    }
+
+    // スクロールの現在地
+    let scrollPosition: number = window.scrollY;
+
+    // 進捗状況を初期化
+    let progress: number = 0;
+
+    if (scrollArea > 0) {
+      // スクロールエリアが0ではなければ、進捗割合を計算
+      progress = scrollPosition / scrollArea;
+    }
+
+    let list = {
+      "scrollPosition": scrollPosition,
+      "scrollArea": scrollArea,
+      "document": htmlHeight,
+      "window": windowY,
+      "progress": progress,
+    };
+
+    return list;
+  }
 }
 
 export default new ScrollCalc();
